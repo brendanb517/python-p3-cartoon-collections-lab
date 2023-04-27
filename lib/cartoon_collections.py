0dce53c68ffb855b4187003cd416f22a7464cc4e
Brendan <brendan.b517@gmail.com>
2023-04-27 04:35:24 -0400

    Passed all tests

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,28 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for dwarf in dwarves:
+        num = dwarves.index(dwarf) + 1
+        print(f"{num}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    # formatted_calls = []
+    # for call in planeteer_calls:
+    #     formatted_call = call.capitalize() + "!"
+    #     formatted_calls.append(formatted_call)
+    # return formatted_calls
+    return [call.capitalize() + "!" for call in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(planeteer_calls):
+    for call in planeteer_calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(foods):
+    for food in foods:
+        if food == "cheddar":
+            return food
+        elif food == "gouda":
+            return food
+        elif food == "camembert":
+            return food
+    return None
